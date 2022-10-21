c165e06cd9a7f93176b990ede6ea96fb177130f7
PicGigi <junior.1997@live.it>
2022-10-21 10:18:09 +0200

    prova

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ console.log("Il prezzo del biglietto è", price, "€");
 const age = parseInt(prompt("Quanti anni hai?"));
 if (age < 18) {
     var newprice = parseFloat(price * 0.80).toFixed(2);
-    console.log("Essendo under 18 hai diritto ad uno sconto del 20%. Il prezzo del biglietto è", newprice,"€");
+    console.log("Essendo under 18 hai diritto ad uno sconto del 20%. Il prezzo del biglietto è", newprice, "€");
 }
 else if (age > 65) {
-    var newprice = (price * 0.60);
-    console.log("Essendo over 65 hai diritto ad uno sconto del 40%. Il prezzo del biglietto è", newprice,"€");
+    var newprice = parseFloat(price * 0.60).toFixed(2);
+    console.log("Essendo over 65 hai diritto ad uno sconto del 40%. Il prezzo del biglietto è", newprice, "€");
 }
 else {
     console.log("Non è applicato alcuno sconto")
